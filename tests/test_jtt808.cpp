f6691b62eb6cfd29965d349f56eb3d775bdc6a17
crazypandas <crazypandas@aliyun.com>
2019-09-06 18:41:13 +0800

    test function EncodeForCRMB

--- tests/test_jtt808.cpp
+++ tests/test_jtt808.cpp
@@ -139,3 +139,138 @@ BOOST_AUTO_TEST_CASE(failed_because_invalidate_phone_number)
 }
 BOOST_AUTO_TEST_SUITE_END()
 
+BOOST_AUTO_TEST_SUITE(DecodePhoneNumber_Test)
+BOOST_AUTO_TEST_CASE(_should_decode_as_expect)
+{
+    BYTE raw[10] = {0x0, 0x0, 0x0,0x0, /*01*/ 0x01, /*52*/ 0x52, /*77*/ 0x77,
+        /*36*/0x36, /*25*/0x25, /*81*/ 0x81
+    };
+    char res[13];
+    const char *exp = "015277362581";
+    JTT_ERROR err = ERR_NONE;
+    err = DecodePhoneNumber(raw, res);
+    BOOST_TEST_REQUIRE(err == ERR_NONE);
+    BOOST_TEST_REQUIRE(res[12] == '\0');
+    BOOST_TEST_REQUIRE(exp == res, "expect: " << exp << ", but res: " << res);
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(EncodeForCRMB_Test)
+    //  0,1-消息ID; 2,3-消息体属性； 4,5,6,7,8,9-终端手机号
+    // 10,11,-消息流水号; 
+    // 前提：有消息包封装项 =》12,13,14,15-消息包封装项; 
+    // 16至检验码前一字节-消息体
+    // 前提：无消息包封装项 12至检验码前一字节-消息体
+BOOST_AUTO_TEST_CASE(_should_encode_as_expect_without_subpackage1)
+{
+    CommonRespMsgBody crmb;
+    BYTE binarySeq[20] = {0x0, 0x0, 0x0, 0x0, 0x0,
+                            0x0, 0x0, 0x0, 0x0, 0x0, 
+                        //  10， 11，  12， 13， 14
+                            0x0, 0x0, 0x0, 0x0, 0x0,
+                        //  15，  16  , 17 , 18, 19
+                            0x0, 0x0, 0x0, 0x0, 0x0};
+
+    BYTE exp[20] = {0x0, 0x0, 0x0, 0x0, 0x0,
+                    0x0, 0x0, 0x0, 0x0, 0x0, 
+                //  10， 11，  12， 13， 14
+                    0x0, 0x0, 0x0, 0x1, 0x0,
+                    0x1};
+    crmb.replyFlowId = 1;
+    crmb.replyId     = 1;
+    crmb.replyCode   = CRR_SUCCESS;
+    EncodeForCRMB(&crmb,  binarySeq);
+    BOOST_TEST_REQUIRE(exp == binarySeq, tt::per_element());
+
+}
+BOOST_AUTO_TEST_CASE(_should_encode_as_expect_without_subpackage2)
+{
+    CommonRespMsgBody crmb;
+    BYTE binarySeq[20] = {0x0, 0x0, 0x0, 0x0, 0x0,
+                            0x0, 0x0, 0x0, 0x0, 0x0, 
+                        //  10， 11，  12， 13， 14
+                            0x0, 0x0, 0x0, 0x0, 0x0,
+                        //  15，  16  , 17 , 18, 19
+                            0x0, 0x0, 0x0, 0x0, 0x0};
+    BYTE exp[20] = {0x0, 0x0, 0x0, 0x0, 0x0,
+                    0x0, 0x0, 0x0, 0x0, 0x0, 
+                //  10， 11，  12， 13， 14
+                    0x0, 0x0, 0x0, 0x64, 0x0,
+                    0x64, 0x1};
+    crmb.replyFlowId = 100;
+    crmb.replyId     = 100;
+    crmb.replyCode   = CRR_FAILED;
+    EncodeForCRMB(&crmb,  binarySeq);
+    BOOST_TEST_REQUIRE(exp == binarySeq, tt::per_element());
+
+}
+BOOST_AUTO_TEST_CASE(_should_encode_as_expect_without_subpackage3)
+{
+    CommonRespMsgBody crmb;
+    BYTE binarySeq[20] = {0x0, 0x0, 0x0, 0x0, 0x0,
+                            0x0, 0x0, 0x0, 0x0, 0x0, 
+                        //  10， 11，  12， 13， 14
+                            0x0, 0x0, 0x0, 0x0, 0x0,
+                        //  15，  16  , 17 , 18, 19
+                            0x0, 0x0, 0x0, 0x0, 0x0};
+    BYTE exp[20] = {0x0, 0x0, 0x0, 0x0, 0x0,
+                    0x0, 0x0, 0x0, 0x0, 0x0, 
+                    0x0, 0x0, 0x0, 0x64, 0xff,
+                    0xff, 0x1};
+    crmb.replyFlowId = 100;
+    crmb.replyId     = 65535;
+    crmb.replyCode   = CRR_FAILED;
+    EncodeForCRMB(&crmb,  binarySeq);
+    BOOST_TEST_REQUIRE(exp == binarySeq, tt::per_element());
+
+}
+
+BOOST_AUTO_TEST_CASE(_should_encode_as_expect_with_subpackage1)
+{
+    CommonRespMsgBody crmb;
+
+    BYTE binarySeq[30] = {0x0, 0x0, 0x24, 0x0, 0x0,
+                            0x0, 0x0, 0x0, 0x0, 0x0, 
+                        //  10， 11，  12， 13， 14
+                            0x0, 0x0, 0x0, 0x0, 0x0,
+                        //  15，  16  , 17 , 18, 19
+                            0x0, 0x0, 0x0, 0x0, 0x0};
+
+    BYTE exp[30] = {0x0, 0x0, 0x24, 0x0, 0x0,
+                    0x0, 0x0, 0x0, 0x0, 0x0,
+                    0x0, 0x0, 0x0, 0x0, 0x0,
+                //  15，  16  , 17 , 18, 19
+                    0x0, 0x0, 0x1, 0x0, 0x1};
+    crmb.replyFlowId = 1;
+    crmb.replyId     = 1;
+    crmb.replyCode   = CRR_SUCCESS;
+    EncodeForCRMB(&crmb,  binarySeq);
+    BOOST_TEST_REQUIRE(exp == binarySeq, tt::per_element());
+
+}
+
+BOOST_AUTO_TEST_CASE(_should_encode_as_expect_with_subpackage2)
+{
+    CommonRespMsgBody crmb;
+    BYTE binarySeq[30] = {0x0, 0x0, 0x24, 0x0, 0x0,
+                            0x0, 0x0, 0x0, 0x0, 0x0, 
+                        //  10， 11，  12， 13， 14
+                            0x0, 0x0, 0x0, 0x0, 0x0,
+                        //  15，  16  , 17 , 18, 19
+                            0x0, 0x0, 0x0, 0x0, 0x0};
+    BYTE exp[30] = {0x0, 0x0, 0x24, 0x0, 0x0,
+                    0x0, 0x0, 0x0, 0x0, 0x0,
+                    0x0, 0x0, 0x0, 0x0, 0x0,
+                //  15，  16  , 17 , 18, 19
+                    0x0, 0x0, 0x64, 0xff, 0xff};
+    crmb.replyFlowId = 100;
+    crmb.replyId     = 65535;
+    crmb.replyCode   = CRR_SUCCESS;
+    EncodeForCRMB(&crmb,  binarySeq);
+    BOOST_TEST_REQUIRE(exp == binarySeq, tt::per_element());
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
